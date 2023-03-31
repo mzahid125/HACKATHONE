146f858c6f09cea1eb8d8361e53155b1948ae6bb
mzahid125 <128369155+mzahid125@users.noreply.github.com>
2023-03-31 23:28:44 +0500

    Add files via upload

---
+++ hackathon/java.js
@@ -0,0 +1,132 @@
+// var a = 5;
+// var b = 10 
+// var c= a + b;
+
+// console.log(c)
+
+// var x = 30;
+// var y = 40
+// var z = x - y; 
+
+// console.log (z)
+
+
+
+// var firstname = prompt("enter your first name")
+// var lastname = prompt("enter your last name")
+// var password = prompt("enter your pasword name")
+// var Account = prompt("enter your Account name")
+
+//console.log (firstname + lastname + password + Account)
+
+alert("WELCOME");
+ window.alert("HELLO WORLD");
+console.log("HELLO WORL");
+
+var title = "jAVA PRACKTICE";
+ console.log(title)
+
+ var x = 10; 
+ var y = 20;
+ var z = x + y;
+ console.log(z)
+
+ var a = 10;
+ var b = 20;
+ var c = b - a;
+ console.log(c)
+
+ var a = 10;
+ var b = 20;
+ var c = b / a;
+ console.log(c)
+
+ var a = 10;
+ var b = 20;
+ var c = 30;
+ var x = a + b - c;
+ console.log(x)
+
+ var a = 10;
+ var b = 20;
+ var c = 40;
+ var x = (a + b) - c;
+ console.log(x)
+
+ var a = 20;
+ var b = 30;
+ var c = 40;
+ var d = 50;
+var x = a + b - c * a / a;
+ console.log(x);
+
+ var a = "10"
+ var b = 20;
+ var c = b - a
+ console.log(c)
+
+ var a = "10abc"
+ var b = 20;
+ var c = b - a
+ console.log(c)
+
+ var x = "Mnm "
+ var y = "Asif"
+ var z = x + y
+console.log(z)
+
+ var a = "10"
+ var b = 20;
+ var c = b - a
+ var c = b * a
+ var c = b / a
+ var c = b + a   
+ console.log(c)
+
+ var a = 10;
+ a = 10 + 1;
+ a++;
+ ++a
+ a--
+--a
+ var b = a++;
+ var b = ++a;
+ console.log(b)
+
+
+ var a = 10;
+ var b = a--;
+ var b = --a;
+ console.log(b)
+
+
+ var firstname= prompt("Enter your name");
+ var lastname= prompt("Enter last name");
+ var city= prompt("Enter city");
+
+ console.log(firstname + " " + lastname + " From " + city)
+
+ var firstname= prompt("Enter your name");
+ var lastname= prompt("Enter last name");
+ var city= prompt("Enter city","Karachi");
+ console.log(firstname + " " + lastname + " From " + city)
+
+ var name = prompt("Enter name")
+ var email = prompt("Enter Email")
+ var phone = prompt("Phone no")
+ var city = prompt("Enter City","Karachi")
+ console.log(name + " " + email + " " + phone + " +" + city)
+
+
+ var Zahid = false;
+ if (Zahid ==true)
+ { console.log("school jao")}
+ else {console.log("school Ni Jao")}
+
+ var iftaaarpartyhe = false;
+ if(iftaaarpartyhe == false) { console.log("fruits le aana") }
+ else{console.log("fruits mt laana")}
+
+ var age = false;
+ if(age == false){console.log("20")}
+ else{console.log("the persons above or below 20 are not allowed")}
